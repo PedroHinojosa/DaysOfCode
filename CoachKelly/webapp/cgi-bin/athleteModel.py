185f3f6432d698d3a779a13c14d96cf777a4473d
PedroHinojosa <petehinojosa85@gmail.com>
2021-12-30 22:03:52 -0600

    Converting to CGI

---
+++ CoachKelly/webapp/cgi-bin/athleteModel.py
@@ -0,0 +1,42 @@
+import pickle
+
+from athleteData import Athlete
+
+def get_coach_data(filename):
+    """This function takes a text file containing player time as input. Returns a dictionary with Name, DOB, and fastest times"""
+    try:
+        with open(filename, "r") as file:   #Reads in the file, ensures it's formatted properly
+            data = file.readline()
+        data = data.strip().split(",")
+        
+        return (Athlete(data.pop(0), data.pop(0), data))
+
+    except IOError as err:
+        print ("File error: " + str(err))
+
+def put_to_store(files_list):
+    all_athletes = {}
+    for each_file in files_list:
+        ath = get_coach_data(each_file)
+        all_athletes[ath.name] = ath
+    try:
+        with open("athletes.pickle", "wb") as athf:
+            pickle.dump(all_athletes, athf)
+    except IOError as ioerr:
+        print("File error (put_and_store):" + str(ioerr))
+    return(all_athletes)
+
+def get_from_store():
+    all_athletes = {}
+    try:
+        with open("athletes.pickle","rb") as athf:
+            all_athletes = pickle.load(athf)
+    except IOError as ioerr:
+        print("File error (get_from_store):" + str(ioerr))
+    return(all_athletes)
+
+the_files =["sarah2.txt", "james2.txt"]
+data = put_to_store(the_files)
+
+print(data)
+    
